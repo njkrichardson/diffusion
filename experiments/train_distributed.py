b7856e6898d083bfb8dfdd5cfe8e9eb0bb5f879c
njkr <njkrichardson@princeton.edu>
2023-12-04 18:27:20 -0500

    factors out trainer for train_distributed

--- experiments/train_distributed.py
+++ experiments/train_distributed.py
@@ -45,152 +45,6 @@ parser.add_argument("--num-channels", type=int, default=1)
 parser.add_argument("--image-size", type=int, default=64)
 parser.add_argument("--frames-per-video", type=int, default=20)
 
-class Trainer:
-    def __init__(
-        self,
-        diffusion_model,
-        folder,
-        *,
-        ema_decay = 0.995,
-        num_frames = 16,
-        train_batch_size = 32,
-        train_lr = 1e-4,
-        train_num_steps = 100000,
-        gradient_accumulate_every = 2,
-        amp = False,
-        step_start_ema = 2000,
-        update_ema_every = 10,
-        save_and_sample_every = 1000,
-        results_folder = './results',
-        num_sample_rows = 4,
-        max_grad_norm = None
-    ):
-        super().__init__()
-        self.model = diffusion_model
-        self.ema = EMA(ema_decay)
-        self.ema_model = copy.deepcopy(self.model)
-        self.update_ema_every = update_ema_every
-
-        self.step_start_ema = step_start_ema
-        self.save_and_sample_every = save_and_sample_every
-
-        self.batch_size = train_batch_size
-        self.image_size = diffusion_model.image_size
-        self.gradient_accumulate_every = gradient_accumulate_every
-        self.train_num_steps = train_num_steps
-
-        image_size = diffusion_model.image_size
-        channels = diffusion_model.channels
-        num_frames = diffusion_model.num_frames
-
-        self.ds = ControlDataset(folder, image_size, channels = channels, num_frames = num_frames)
-
-        print(f'found {len(self.ds)} videos as gif files at {folder}')
-        assert len(self.ds) > 0, 'need to have at least 1 video to start training (although 1 is not great, try 100k)'
-
-        self.dl = cycle(DataLoader(self.ds, batch_size = train_batch_size, shuffle=True, pin_memory=True))
-        self.opt = Adam(diffusion_model.parameters(), lr = train_lr)
-
-        self.step = 0
-
-        self.amp = amp
-        self.scaler = GradScaler(enabled = amp)
-        self.max_grad_norm = max_grad_norm
-
-        self.num_sample_rows = num_sample_rows
-        self.results_folder = Path(results_folder)
-        self.results_folder.mkdir(exist_ok = True, parents = True)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.ema_model.load_state_dict(self.model.state_dict())
-
-    def step_ema(self):
-        if self.step < self.step_start_ema:
-            self.reset_parameters()
-            return
-        self.ema.update_model_average(self.ema_model, self.model)
-
-    def save(self, milestone):
-        data = {
-            'step': self.step,
-            'model': self.model.state_dict(),
-            'ema': self.ema_model.state_dict(),
-            'scaler': self.scaler.state_dict()
-        }
-        torch.save(data, str(self.results_folder / f'model-{milestone}.pt'))
-
-    def load(self, milestone, **kwargs):
-        if milestone == -1:
-            all_milestones = [int(p.stem.split('-')[-1]) for p in Path(self.results_folder).glob('**/*.pt')]
-            assert len(all_milestones) > 0, 'need to have at least one milestone to load from latest checkpoint (milestone == -1)'
-            milestone = max(all_milestones)
-
-        data = torch.load(str(self.results_folder / f'model-{milestone}.pt'))
-
-        self.step = data['step']
-        self.model.load_state_dict(data['model'], **kwargs)
-        self.ema_model.load_state_dict(data['ema'], **kwargs)
-        self.scaler.load_state_dict(data['scaler'])
-
-    def train(
-        self,
-        prob_focus_present = 0.,
-        focus_present_mask = None,
-        log_fn = noop, 
-        writer = None
-    ):
-        assert callable(log_fn)
-
-        while self.step < self.train_num_steps:
-            for i in range(self.gradient_accumulate_every):
-                data = next(self.dl).cuda()
-
-                with autocast(enabled = self.amp):
-                    loss = self.model(
-                        data,
-                        prob_focus_present = prob_focus_present,
-                        focus_present_mask = focus_present_mask
-                    )
-
-                    self.scaler.scale(loss / self.gradient_accumulate_every).backward()
-
-            log_fn(f'{self.step}: {loss.item()}')
-            
-            if writer is not None: 
-                writer.scalar("Objective", loss.item(), step=self.step)
-
-            if exists(self.max_grad_norm):
-                self.scaler.unscale_(self.opt)
-                nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
-
-            self.scaler.step(self.opt)
-            self.scaler.update()
-            self.opt.zero_grad()
-
-            if self.step % self.update_ema_every == 0:
-                self.step_ema()
-
-            if self.step != 0 and self.step % self.save_and_sample_every == 0:
-                milestone = self.step // self.save_and_sample_every
-                num_samples = self.num_sample_rows ** 2
-                batches = num_to_groups(num_samples, self.batch_size)
-
-                all_videos_list = list(map(lambda n: self.ema_model.sample(batch_size=n), batches))
-                all_videos_list = torch.cat(all_videos_list, dim = 0)
-
-                all_videos_list = F.pad(all_videos_list, (2, 2, 2, 2))
-
-                one_gif = rearrange(all_videos_list, '(i j) c f h w -> c f (i h) (j w)', i = self.num_sample_rows)
-                video_path = str(self.results_folder / str(f'{milestone}.gif'))
-                video_tensor_to_gif(one_gif, video_path)
-                self.save(milestone)
-
-            self.step += 1
-
-        log_fn('training completed')
-
 def main(args): 
     # configure logging 
     experiment_directory: Path = setup_experiment_directory("control_unconditional")
@@ -225,9 +79,10 @@ def main(args):
     ema_model.load_state_dict(model.state_dict())
 
     # configure dataset 
-    dataset: Dataset = ControlDataset(args.data_dir, args.image_size, channels=args.num_channels, num_frames=args.frames_per_video)
+    data_directory = DATA_DIRECTORY / f"{args.data_dir}"
+    dataset: Dataset = ControlDataset(data_directory, args.image_size, channels=args.num_channels, num_frames=args.frames_per_video)
     num_videos: int = len(dataset)
-    log.info(f"Found {num_videos=} videos at {args.data_dir=}")
+    log.info(f"Found {num_videos=} videos at {data_directory=}")
     assert num_videos >= 1 
     dataloader: DataLoader = cycle(DataLoader(dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True))
 
@@ -284,9 +139,9 @@ def main(args):
                 'step': step,
                 'model': model.state_dict(),
                 'ema': ema_model.state_dict(),
-                'scaler': scaler.state_dict()
+                'scaler': gradient_scaler.state_dict()
             }
-            torch.save(data, str(experiment_directory / f'checkpoint-{milestone}.pt'))
+            torch.save(checkpoint_data, str(experiment_directory / f'checkpoint-{milestone}.pt'))
 
         step += 1
 
